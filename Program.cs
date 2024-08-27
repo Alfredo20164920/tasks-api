980ef4199e7d767d580ca2eade3068c6def59895
Alfredo20164920 <apaz20164920@gmail.com>
2024-08-26 20:37:14 -0600

    Connect EF with db in memory

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using ef_tasks.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TasksContext>((otp) => otp.UseInMemoryDatabase("TasksDB"));   
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/dbconnection",  ([FromServices] TasksContext dbCtx) => {
+    dbCtx.Database.EnsureCreated();
+    return Results.Ok("Data Base created: " + dbCtx.Database.IsInMemory());
+});
 
 app.Run();
